f42245dbb77ac21696fe1bed38282cc2bee8a978
M1shgan48 <118652349+M1shgan48@users.noreply.github.com>
2022-12-28 16:35:14 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,38 +1,15 @@
 import os
 import sys
 import pygame
-from pygame.locals import *
-
 
 pygame.init()
-size = width, height = 800, 600
+size = width, height = 800, 800
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-running = True
-font = pygame.font.SysFont('Arial', 40)
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
-objects = []
-
-class Button():
-    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.onclickFunction = onclickFunction
-        self.onePress = onePress
-        self.alreadyPressed = False
-
-        self.fillColors = {
-            'normal': '#ffffff',
-            'hover': '#666666',
-            'pressed': '#333333',
-        }
-        self.buttonSurface = pygame.Surface((self.width, self.height))
-        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
-
-        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
-        objects.append(self)
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('C:\pythonProject3\data', name)
@@ -55,39 +32,110 @@ def terminate():
     sys.exit()
 
 
-def start_screen():
+    def start_screen():
+        intro_text = ["ЗАСТАВКА", "",
+                      "Правила игры",
+                      "Если в правилах несколько строк,",
+                      "приходится выводить их построчно"]
 
-    fon = pygame.transform.scale(load_image('fon_game.jpg'), (width, height))
-    screen.blit(fon, (0, 0))
+        fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+        screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, True, pygame.Color('white'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            screen.blit(string_rendered, intro_rect)
 
-    while True:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            clock.tick(40)
+
+
+    def load_level(filename):
+        filename = "C:\pythonProject3\data/" + filename
+        # читаем уровень, убирая символы перевода строки
+        with open(filename, 'r') as mapFile:
+            level_map = [line.strip() for line in mapFile]
+
+        # и подсчитываем максимальную длину
+        max_width = max(map(len, level_map))
+
+        # дополняем каждую строку пустыми клетками ('.')
+        return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+    tile_images = {
+        'wall': load_image('box.png'),
+        'empty': load_image('grass.png')
+    }
+    player_image = load_image('mario.png')
+
+    tile_width = tile_height = 50
+
+
+    class Tile(pygame.sprite.Sprite):
+        def __init__(self, tile_type, pos_x, pos_y):
+            super().__init__(tiles_group, all_sprites)
+            self.image = tile_images[tile_type]
+            self.rect = self.image.get_rect().move(
+                tile_width * pos_x, tile_height * pos_y)
+
+
+    class Player(pygame.sprite.Sprite):
+        def __init__(self, pos_x, pos_y):
+            super().__init__(player_group, all_sprites)
+            self.image = player_image
+            self.rect = self.image.get_rect().move(
+                tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+
+    def generate_level(level):
+        new_player, x, y = None, None, None
+        for y in range(len(level)):
+            for x in range(len(level[y])):
+                if level[y][x] == '.':
+                    Tile('empty', x, y)
+                elif level[y][x] == '#':
+                    Tile('wall', x, y)
+                elif level[y][x] == '@':
+                    Tile('empty', x, y)
+                    new_player = Player(x, y)
+        # вернем игрока, а также размер поля в клетках
+        return new_player, x, y
+
+
+    level = load_level('map.txt')
+    player, x, y = generate_level(level)
+    start_screen()
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                running = False
+            if event.type == pygame.KEYDOWN:
+                moves = pygame.key.get_pressed()
+                if moves[pygame.K_w]:
+                    player.rect.y -= tile_height
+                if moves[pygame.K_s]:
+                    player.rect.y += tile_height
+                if moves[pygame.K_a]:
+                    player.rect.x -= tile_width
+                if moves[pygame.K_d]:
+                    player.rect.x += tile_width
+        screen.fill(pygame.Color("black"))
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+        clock.tick(40)
         pygame.display.flip()
-        clock.tick(60)
-
-def myFunction():
-    print('Button Pressed')
-
-
-customButton = (Button(30, 30, 400, 100, 'Button One (onePress)', myFunction))
-customButton1 = Button(30, 140, 400, 100, 'Button Two (multiPress)', myFunction, True)
-
-start_screen()
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    for object in objects:
-        object.process()
-
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
-    clock.tick(60)
-
-pygame.quit()
+    pygame.quit()
