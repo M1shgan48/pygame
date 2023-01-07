8aa85e3bcf17f874e1ed221d198dad1854ebe32f
M1shgan48 <118652349+M1shgan48@users.noreply.github.com>
2023-01-07 21:48:00 +0300

    Update game.py

--- game.py
+++ game.py
@@ -53,22 +53,32 @@ def load_image(name):
     return image
 
 def generate_level(level):
+    global x2, y2
     new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '*':
-                Tile('empty', x, y)
-            elif level[y][x] == 'X':
-                Tile('wall', x, y)
-            elif level[y][x] == '0':
-                Tile('lakes', x, y)
-            elif level[y][x] == 'C':
-                Tile('city', x, y)
-            elif level[y][x] == 'W':
-                Tile('swamp', x, y)
-            elif level[y][x] == 'B':
-                Tile('bad_emty', x, y)
-    # вернем игрока, а также размер поля в клетках
+    if flag == 0:
+        for y in range(len(level)):
+            for x in range(len(level[y])):
+                if level[y][x] == '*':
+                    Tile('empty', x, y)
+                elif level[y][x] == 'X':
+                    Tile('wall', x, y)
+                elif level[y][x] == '0':
+                    Tile('lakes', x, y)
+                elif level[y][x] == 'C':
+                    Tile('city', x, y)
+                elif level[y][x] == 'W':
+                    Tile('swamp', x, y)
+                elif level[y][x] == 'B':
+                    Tile('bad_emty', x, y)
+                elif level[y][x] == 'q':
+                    Tile("fish_house", x, y)
+                elif level[y][x] == 'w':
+                    Tile("tree_house", x, y)
+                elif level[y][x] == 'e':
+                    Tile("house", x, y)
+                elif level[y][x] == 'r':
+                    Tile("church", x, y)
+        # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
 def myFunction():
@@ -78,8 +88,9 @@ def myFunction():
 def load_level(filename):
     filename = "C:\pythonProject3\pygame_data/" + filename
     # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
+    with open(filename, 'r+') as mapFile:
         level_map = [line.strip() for line in mapFile]
+
 
     # и подсчитываем максимальную длину
     max_width = max(map(len, level_map))
@@ -94,7 +105,11 @@ tile_images = {
     'lakes': load_image('озеро.png'),
     'swamp': load_image('болото.png'),
     'city': load_image('замок.png'),
-    'bad_emty': load_image('неплодородная_земля.png')
+    'bad_emty': load_image('неплодородная_земля.png'),
+    'fish_house': load_image('рыбацкий дом2.png'),
+    'tree_house': load_image('лесопилка2.png'),
+    'house': load_image('дом2.png'),
+    'church': load_image('церковь2.png')
 }
 
 tile_width = tile_height = 50
@@ -103,8 +118,9 @@ tile_width = tile_height = 50
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
-
-
+flag = 0
+flag_button = 0
+flag2 = 0
 
 if __name__ == '__main__':
     pygame.init()
@@ -125,21 +141,53 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN and flag == 0:
                 pos = pygame.mouse.get_pos()
                 if (pos[0] >= 410 and pos[0] <= 575) and (pos[1] >= 730 and pos[1] <= 780):
                     player, level_x, level_y = generate_level(load_level('first_level.txt'))
                     screen.fill("black")
                     tiles_group.draw(screen)
+                    flag = 1
+                    flag2 = 1
                 elif (pos[0] >= 410 and pos[0] <= 575) and (pos[1] >= 650 and pos[1] <= 700):
                     player, level_x, level_y = generate_level(load_level('second_level.txt'))
                     screen.fill("black")
                     tiles_group.draw(screen)
+                    flag = 1
+                    flag2 = 2
                 elif (pos[0] >= 410 and pos[0] <= 575) and (pos[1] >= 570 and pos[1] <= 620):
                     player, level_x, level_y = generate_level(load_level('level_three.txt'))
                     screen.fill("black")
                     tiles_group.draw(screen)
+                    flag = 1
+                    flag2 = 3
+            if flag_button != 0 and event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                x2, y2 = pos
+                x2, y2 = (x2 - 0) // 50, (y2 - 0) // 50
+                if flag_button == 1:
+                    Tile("fish_house", x2, y2)
+                if flag_button == 1:
+                    Tile("tree_house", x2, y2)
+                if flag_button == 2:
+                    Tile("fish_house", x2, y2)
+                if flag_button == 3:
+                    Tile("house", x2, y2)
+                if flag_button == 4:
+                    Tile("church", x2, y2)
+                flag_button = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    flag_button = 1
+                elif event.key == pygame.K_w:
+                    flag_button = 2
+                elif event.key == pygame.K_e:
+                    flag_button = 3
+                elif event.key == pygame.K_r:
+                    flag_button = 4
+
         pygame.display.flip()
         clock.tick(60)
-
+        all_sprites.draw(screen)
+        all_sprites.update()
     pygame.quit()
